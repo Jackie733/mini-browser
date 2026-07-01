f2b9a45bad3906bb31655b5963edf0e3ef16658d
Tsien <jackie10673311@gmail.com>
2026-07-01 18:07:53 +0800

    feat: implement layout and scroll in lab2, add webtools

--- src/lab2.py
+++ src/lab2.py
@@ -1,14 +1,39 @@
 import tkinter
 
+import webtools
 from lab1 import URL
 
 WIDTH, HEIGHT = 800, 600
+HSTEP, VSTEP = 13, 18
+SCROLL_STEP = 100
+
 
 def lex(body):
     text = ""
-    
+    in_tag = False
     for c in body:
-        #
+        if c == "<":
+            in_tag = True
+        elif c == ">":
+            in_tag = False
+        elif not in_tag:
+            text += c
+        webtools.record("lex", text)
+    return text
+
+
+def layout(text):
+    display_list = []
+    cursor_x, cursor_y = HSTEP, VSTEP
+    for c in text:
+        display_list.append((cursor_x, cursor_y, c))
+        cursor_x += HSTEP
+        if cursor_x >= WIDTH - HSTEP:
+            cursor_y += VSTEP
+            cursor_x = HSTEP
+        webtools.record("layout", display_list)
+    return display_list
+
 
 class Browser:
     def __init__(self):
@@ -16,10 +41,28 @@ class Browser:
         self.canvas = tkinter.Canvas(self.window, width=WIDTH, height=HEIGHT)
         self.canvas.pack()
 
+        self.scroll = 0
+
+        self.window.bind("<Down>", self.scrolldown)
+
+    def draw(self):
+        self.canvas.delete("all")
+        for x, y, c in self.display_list:
+            if y > self.scroll + HEIGHT:
+                continue
+            if y + VSTEP < self.scroll:
+                continue
+            self.canvas.create_text(x, y - self.scroll, text=c)
+
+    def scrolldown(self, e):
+        self.scroll += SCROLL_STEP
+        self.draw()
+
     def load(self, url):
-        self.canvas.create_rectangle(10, 20, 400, 300)
-        self.canvas.create_oval(100, 100, 150, 150)
-        self.canvas.create_text(200, 150, text="Hi!")
+        body = url.request()
+        text = lex(body)
+        self.display_list = layout(text)
+        self.draw()
 
 
 if __name__ == "__main__":
